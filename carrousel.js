e88ab6a9922d393b1566e486f347c6055a3da143
axelle <kadance9720@gmail.com>
2021-03-15 18:41:12 +0100

    Automatisation du carrousel

--- carrousel.js
+++ carrousel.js
@@ -3,10 +3,17 @@ $(function () {
     var slider = $('#slider'),
         img = $('#slider img');
         indexImg = img.length - 1,
-        i = 0,
-        currentImg = img.eq(i);
+        i = 0, // Initialisation compteur
+        currentImg = img.eq(i);  // Ciblier l'image courante, qui possède l'index i (0 pour l'instant)
 
-    slider.prepend('<button class="prev">prev</button> <button class="next">next</button>');
+    slider.prepend('<i id="nav-prev" class="las la-chevron-left"></i><i id="nav-next" class="las la-chevron-right"></i>');
+
+    var btnP = $('#nav-prev'),
+        btnN = $('#nav-next');
+
+    btnP.css({
+        
+    });
 
     img.css('display', 'none');
     currentImg.css('display', 'block');
@@ -38,5 +45,23 @@ $(function () {
             i = 0;
         }
     });
+
+    function slide(){
+        setTimeout(function() {
+            if (i < indexImg) {  // Si compteur inférieur au dernier index
+                i++; // on l'incrémente
+            } else { // sinon, remis à 0 (première image)
+                i = 0;
+            }
+
+            img.css('display', 'none');
+            currentImg = img.eq(i);
+            currentImg.css('display', 'block');
+
+            slide(); //on relance la fonction
+        }, 2000);
+    }
+
+    slide();
 
 })
